ac1ea6f97adce156a68f0f139d52ebd4b4d8e50b
Sarah Thompson <sthompson@fintechprocessing.com>
2022-04-23 11:44:29 -0400

    everything working except the delete note

---
+++ server.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path')
+const express = require('express')
+let store = require('./db/db.json');
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+// ######################API#####################
+app.get('/api/notes', (req, res) => {
+    res.json(store);
+});
+app.post('/api/notes', (req, res) =>{
+    store.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(store), err=> {
+        if (err) throw err;
+    })
+    res.json(store);
+})
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
