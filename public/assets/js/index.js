646c8e3308bd5e5940375b8d9941484567f9ac91
Sarah Thompson <sthompson@fintechprocessing.com>
2022-04-25 10:58:34 -0400

    organized code in server.js to see where the issue with delete function is

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -70,6 +70,7 @@ const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
     text: noteText.value,
+    id: idText.value,
   };
   saveNote(newNote).then(() => {
     getAndRenderNotes();
--- server.js
+++ server.js
@@ -4,20 +4,37 @@ const express = require('express')
 let store = require('./db/db.json');
 const { Router } = require('express');
 
-const uniqueId = require('uniqueId');
-const { findById, findByIdAndDelete } = require('./db/db.json');
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+const findById = (id, notesArray) => {
+    const result = notesArray.filter(note => note.id === id)[0];
+
+    return result;
+}
+
+const findByIdAndDelete = (id, notesArray) => {
+   const removeIndex = notesArray.map((item) => { return item.id }).indexOf(id);
+   notesArray.splice(removeIndex, 1);
+   return removeIndex;
+}
+
+module.exports = {
+    findById,
+    findByIdAndDelete
+}
+
+//use
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+//Get routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-// ######################API#####################
+// ##################   API   ################
 app.get('/api/notes', (req, res) => {
     res.json(store);
 });
@@ -28,8 +45,8 @@ app.get('api/notes/:id', (req, res) => {
     res.send(result);
 });
 
+//post route
 app.post('/api/notes', (req, res) =>{
-    req.body.id = uniqueId();
     store.push(req.body);
     fs.writeFile('./db/db.json', JSON.stringify(store), err=> {
         if (err) throw err;
@@ -37,6 +54,7 @@ app.post('/api/notes', (req, res) =>{
     res.json(store);
 })
 
+//delete note
 app.delete('/api/notes.:id', (req, res) => {
     const { id } = req.params;
     const result = findByIdAndDelete(id, store);
@@ -45,3 +63,4 @@ app.delete('/api/notes.:id', (req, res) => {
 
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+
